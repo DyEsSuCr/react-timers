f9c75318b488af402cec0a747ceb9c94266ad1ee
DylanSuarez <2000sajo@gmail.com>
2023-07-30 00:33:23 -0500

    timerContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
 
+import { TimerProvider } from './context/timerContext'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TimerProvider>
+      <App />
+    </TimerProvider>
   </React.StrictMode>
 )
