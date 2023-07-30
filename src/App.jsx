c1853d6f2619f48d22f8919a75c6955a681390f3
DylanSuarez <2000sajo@gmail.com>
2023-07-30 00:34:18 -0500

    AppTimer - APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { AppTimer } from './components/AppTimer'
+import { Clock } from './components/clock/Clock'
+import { Form } from './components/form/Form'
+import { Timers } from './components/timers/Timers'
 
-function App() {
+export default function App () {
   return (
-    <>
-      <AppTimer />
-    </>
+    <div className='min-h-screen w-full bg-primary text-white relative'>
+      <Clock />
+      <Form />
+      <Timers />
+    </div>
   )
 }
-
-export default App
